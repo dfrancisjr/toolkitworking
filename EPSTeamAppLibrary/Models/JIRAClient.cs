89e1ef09b289d2f7074e238c56910c64f314327d
David Francis <dfrancisjr@gmail.com>
2023-02-28 14:06:06 -0700

    Add files via upload

---
+++ EPSTeamAppLibrary/Models/JIRAClient.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.IO;
+using System.Net;
+
+namespace EPSTeamAppLibrary.Models
+{
+    public enum httpverb
+    {
+        GET,
+        POST,
+        PUT,
+        DELETE
+    }
+
+    public enum authenticationType
+    {
+        Basic,
+        NTLM
+    }
+
+    public enum authenticationTechnique
+    {
+        RollYourOwn,
+        NetworkCredential
+    }
+
+    class JIRAClient
+    {
+        public string endPoint { get; set; }
+        public httpverb httpMethod { get; set; }
+        public authenticationType authType { get; set; }
+        public authenticationTechnique authTech { get; set; }
+        public string userName { get; set; }
+        public string userPassword { get; set; }
+        public string postJSON { get; set; }
+
+        public JIRAClient()
+        {
+            endPoint = string.Empty;
+            httpMethod = httpverb.GET;
+        }
+
+        public string makeRequest()
+        {
+            string strResponseValue = string.Empty;
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(endPoint);
+
+            request.Method = httpMethod.ToString();
+
+            String authHeader = System.Convert.ToBase64String(System.Text.ASCIIEncoding.ASCII.GetBytes(userName + ":" + userPassword));
+            request.Headers.Add("Authorization", authType.ToString() + " " + authHeader);
+
+            if (request.Method == "POST" && postJSON != string.Empty)
+            {
+                request.ContentType = "application/json";
+                using (StreamWriter swJSONPayLoad = new StreamWriter(request.GetRequestStream()))
+                {
+                    swJSONPayLoad.Write(postJSON);
+
+                    swJSONPayLoad.Close();
+                }
+            }
+
+            HttpWebResponse response = null;
+
+            try
+            {
+                response = (HttpWebResponse)request.GetResponse();
+
+                // Process the response stream... (could be JSON, XML or HTML etc...)
+
+                using (Stream responseStream = response.GetResponseStream())
+                {
+                    if (responseStream != null)
+                    {
+                        using (StreamReader reader = new StreamReader(responseStream))
+                        {
+                            strResponseValue = reader.ReadToEnd();
+                        }//End of StreamReader
+                    }
+                }// End of using ResponseStream
+            }// End of using response
+            catch (Exception ex)
+            {
+                strResponseValue = "{\"errorMessages\":[\"" + ex.Message.ToString() + "\"],\"errors\":{}}";
+            }
+            finally
+            {
+                if (response != null)
+                {
+                    ((IDisposable)response).Dispose();
+                }
+            }
+
+            return strResponseValue;
+        }
+    }
+}
